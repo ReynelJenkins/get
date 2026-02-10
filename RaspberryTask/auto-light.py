00ff6457044682a818d1b38f5d2a8a7b712d4606
RKobyshev <kobyshev.ra@phystech.edu>
2026-02-10 15:30:56 +0300

    Raspberry Pi Task

---
+++ RaspberryTask/auto-light.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+state = 0
+led = 26
+GPIO.setup(led, GPIO.OUT)
+photo = 6
+GPIO.setup(photo, GPIO.IN)
+while True:
+    GPIO.output(led, not GPIO.input(photo))
+
